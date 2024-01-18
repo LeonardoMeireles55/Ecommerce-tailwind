bc6c70b862a62c7728e719bf1f9b681dacc76211
leonardo <leomeireles55@hotmail.com>
2024-01-18 15:56:22 -0300

    tweaks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 let productsTech = [
-  { name: 'Smartphone', price: 999.99, image: 'https://source.unsplash.com/featured/250x200?smarthphone', ratingsCount: 2  },
-  { name: 'PC Gamer', price: 2499.99, image: 'https://source.unsplash.com/featured/250x200?computer', ratingsCount: 3  },
-  { name: 'Fone de Ouvido Bluetooth', price: 149.99, image: 'https://source.unsplash.com/featured/250x200?headphone' , ratingsCount: 4 },
-  { name: 'Mouse', price: 49.99, image: 'https://source.unsplash.com/featured/250x200?mouse', ratingsCount: 5  },
-  { name: 'Teclado', price: 79.99, image: 'https://source.unsplash.com/featured/250x200?keyboard', ratingsCount: 2  },
-  { name: 'Joystick', price: 69.99, image: 'https://source.unsplash.com/featured/250x200?joystick', ratingsCount: 1  },
-  { name: 'PS4', price: 299.99, image: 'https://source.unsplash.com/featured/250x200?PS4', ratingsCount: 4 },
-  { name: 'Relógio', price: 129.99, image: 'https://source.unsplash.com/featured/250x200?relogio', ratingsCount: 3 }
+  { name: 'Smartphone', price: 999.99, image: 'https://source.unsplash.com/featured/250x200?smarthphone', ratingsCount: 2, offPrice: 50 },
+  { name: 'PC Gamer', price: 2499.99, image: 'https://source.unsplash.com/featured/250x200?computer', ratingsCount: 3, offPrice: 10  },
+  { name: 'Fone de Ouvido Bluetooth', price: 149.99, image: 'https://source.unsplash.com/featured/250x200?headphone' , ratingsCount: 4, offPrice: 10 },
+  { name: 'Mouse', price: 49.99, image: 'https://source.unsplash.com/featured/250x200?mouse', ratingsCount: 5, offPrice: 10  },
+  { name: 'Teclado', price: 79.99, image: 'https://source.unsplash.com/featured/250x200?keyboard', ratingsCount: 2, offPrice: 10  },
+  { name: 'Joystick', price: 69.99, image: 'https://source.unsplash.com/featured/250x200?joystick', ratingsCount: 1, offPrice: 10 },
+  { name: 'PS4', price: 299.99, image: 'https://source.unsplash.com/featured/250x200?PS4', ratingsCount: 4, offPrice: 10},
+  { name: 'Relógio', price: 129.99, image: 'https://source.unsplash.com/featured/250x200?relogio', ratingsCount: 3, offPrice: 10 }
 ];
 
-function createProductTags(name, price, image, ratingsCount) {
+function createProductTags(name, price, image, ratingsCount, offPrice) {
   let div = document.createElement('div');
   div.className = 'flex flex-col items-center justify-center bg-gray-100 p-6 shadow-xl rounded-md text-center';
 
@@ -23,7 +23,7 @@ function createProductTags(name, price, image, ratingsCount) {
 
   let offPriceElement = document.createElement('p');
   offPriceElement.className = 'text-gray-700 text-md';
-  let discountedPrice = (+price - (+price * 10 / 100)).toFixed(2);
+  let discountedPrice = (+price - (+price * offPrice / 100)).toFixed(2);
   offPriceElement.textContent = 'R$' + discountedPrice;
 
   let imageElement = document.createElement('img');
@@ -51,6 +51,31 @@ function createProductTags(name, price, image, ratingsCount) {
 }
 
 
+//Reduce offPrice
+function totalOffPriceApply(products, off) {
+  const productsOffPrice = products.reduce((total, product) => total + (product.price - (product.price * off / 100)), 0);
+
+  return productsOffPrice
+}
+
+//Map
+function offPriceApply(products) {
+  const productsWithOffPriceList = products.map(product => ({
+    ...product,
+    price: (product.price - (product.price * product.offPrice / 100)).toFixed(2)
+  }));
+  
+  return productsWithOffPriceList;
+}
+
+console.log(offPriceApply(productsTech))
+
+//Filter price range
+function priceRange(products, maxPrice) {
+  const productsFiltered = products.filter(product => product.price < maxPrice);
+  return productsFiltered;
+}
+
 //Sort functions
 function orderProductsByPriceAsc(products) {
   const sortedProducts = products.map((product) => product).sort((a, b) => a.price - b.price);
@@ -64,10 +89,17 @@ function orderProductsByPriceDesc(products) {
 
 // forEach
 orderProductsByPriceDesc(productsTech).forEach((product) => {
-  let productElement = createProductTags(product.name.toUpperCase(), product.price, product.image, product.ratingsCount);
+  let productElement = createProductTags(product.name.toUpperCase(), product.price, product.image, product.ratingsCount, product.offPrice);
 
   document.getElementById('products-list').appendChild(productElement);
 })
+
+// forEach with price range
+// orderProductsByPriceDesc(priceRange(productsTech, 200)).forEach((product) => {
+//   let productElement = createProductTags(product.name.toUpperCase(), product.price, product.image, product.ratingsCount);
+
+//   document.getElementById('products-list').appendChild(productElement);
+// })
 
 // // Classic For
 // for(i = 0; i <= productsTech.length - 1; i++) {
